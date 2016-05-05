336415411833d8ce351ed2053b9b8ac8e761eb6d
Dave Tapley <dukedave@gmail.com>
2016-05-04 19:14:05 -0700

    Customer has many visits

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,4 +1,6 @@
 class Customer < ActiveRecord::Base
   include FriendlyId
   friendly_id :name, use: [:slugged, :history]
+
+  has_many :visits
 end
--- db/migrate/20160428154215_create_visits.rb
+++ db/migrate/20160428154215_create_visits.rb
@@ -1,7 +1,7 @@
 class CreateVisits < ActiveRecord::Migration
   def change
     create_table :visits do |t|
-
+      t.belongs_to :customer
       t.timestamps null: false
     end
   end
