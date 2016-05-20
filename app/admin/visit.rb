29ca0fe2244429a5233bd357f4e0c4b513163817
Dave Tapley <dukedave@gmail.com>
2016-05-19 22:13:42 -0700

    Finish a visit instead of destroying it

--- app/admin/visit.rb
+++ app/admin/visit.rb
@@ -20,6 +20,17 @@ ActiveAdmin.register Visit do
     actions
   end
 
+  member_action :finish, method: :put do
+    resource.update_attribute :departed_at, Time.current
+    redirect_to resource_path, notice: "Visit finished"
+  end
+
+  actions :all, except: [:destroy]
+
+  action_item :finish, only: [:show, :edit] do
+    link_to 'Finish', finish_admin_customer_visit_path(id: resource.id), method: :put if resource.departed_at.nil?
+  end
+
   permit_params :reason
 
   config.batch_actions = false
