9c2766613790d2a3a4091e2598a7616dbfdda984
Dave Tapley <dukedave@gmail.com>
2016-06-01 18:20:19 -0700

    Prompt to log in again after time out

--- app/assets/javascripts/session_timeouts.js
+++ app/assets/javascripts/session_timeouts.js
@@ -3,6 +3,10 @@ var updateLogoutTime = function() {
     $.get('/session_timeout.json', function(data, status){
       $('#logout a').html('Logout (in ' + data.time_left + 's)');
       updateLogoutTime();
+    }).fail(function(response) {
+      if(response.status == 401) {
+        location.reload();
+      }
     });
   }, 1000);
 }
