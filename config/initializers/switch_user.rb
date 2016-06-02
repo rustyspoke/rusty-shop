aaa11eeac2cd0288432f717f706afeecbb1dbc48
Dave Tapley <dukedave@gmail.com>
2016-06-01 18:40:01 -0700

    Allow user switch in production

--- config/initializers/switch_user.rb
+++ config/initializers/switch_user.rb
@@ -24,13 +24,13 @@ SwitchUser.setup do |config|
   # if it returns true, the request will continue,
   # else the request will be refused and returns "Permission Denied"
   # if you switch from "admin" to user, the current_user param is "admin"
-  config.controller_guard = lambda { |current_user, request| Rails.env.development? }
+  config.controller_guard = lambda { |_current_user, _request| true }
 
   # view_guard is a block,
   # if it returns true, the switch user select box will be shown,
   # else the select box will not be shown
   # if you switch from admin to "user", the current_user param is "user"
-  config.view_guard = lambda { |current_user, request| Rails.env.development? }
+  config.view_guard = lambda { |_current_user, _request| true }
 
   # redirect_path is a block, it returns which page will be redirected
   # after switching a user.
