b9e300eba36809759ad03e5751169ac233a9cbc8
Dave Tapley <dukedave@gmail.com>
2016-07-06 19:22:54 -0700

    Ignore weird papertrail audits

--- app/admin/log.rb
+++ app/admin/log.rb
@@ -4,7 +4,13 @@ ActiveAdmin.register_page 'Log' do
   content do
     section "Recently updated content" do
       table_for PaperTrail::Version.order('id desc').limit(20) do # Use PaperTrail::Version if this throws an error
-        column ("Item") { |v| v.item.try(:decorate).try :title }
+        column ("Item") do |v|
+          begin
+            v.item.try(:decorate).try :title
+          rescue
+            nil
+          end
+        end
         column ("Type") { |v| v.item_type.underscore.humanize }
         column ("Modified at") { |v| v.created_at.to_s :long }
         column ("Admin") { |v| v.whodunnit ? AdminUser.find(v.whodunnit).name : nil }
