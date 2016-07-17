c951ed4c6027a46e92ff2e1f16e17811d8a22297
Dave Tapley <dukedave@gmail.com>
2016-07-17 14:31:20 -0700

    Don't send email receipt unless enabled and have email

--- app/jobs/finish_worktrade_visit_job.rb
+++ app/jobs/finish_worktrade_visit_job.rb
@@ -1,9 +1,16 @@
 FinishWorktradeVisitJob = Struct.new(:visit_id) do
   def perform
-    VisitMailer.worktrade_receipt(visit).deliver_now
+    send_receipt
   end
 
   private
+
+  def send_receipt
+    return unless ENV['SEND_WORKTRADE_RECEIPTS'].present?
+    return unless visit.customer.email.present?
+
+    VisitMailer.worktrade_receipt(visit).deliver_now
+  end
 
   def visit
     @visit ||= Visit.find visit_id
