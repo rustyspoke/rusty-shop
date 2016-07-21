865d6462c8ced5ce19f97b4a1d5336e4980bb049
Dave Tapley <dukedave@gmail.com>
2016-07-20 18:47:30 -0700

    Fix donation value

--- app/models/donation.rb
+++ app/models/donation.rb
@@ -1,15 +1,12 @@
 class Donation < ActiveRecord::Base
   belongs_to :customer
 
-  attr_accessor :value
-
-  validate do
-    return if value.blank?
+  def value=(value)
     errors.add :value, "can't parse #{value} as dollar value" unless value.to_f
     self.value_cents = value.to_f * 100.0
   end
 
   def value
-    value_cents / 100.0
+    value_cents.to_f / 100.0
   end
 end
