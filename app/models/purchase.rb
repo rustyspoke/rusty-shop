94842bd687e38dd6c9ad3b46c8c650a922126e76
Dave Tapley <dukedave@gmail.com>
2016-08-21 14:31:16 -0700

    Bug fixes for purchases
    Allow nils.

--- app/models/purchase.rb
+++ app/models/purchase.rb
@@ -4,15 +4,17 @@ class Purchase < ActiveRecord::Base
   belongs_to :customer
   has_paper_trail
 
-  monetize :cost_cash_cents
-  monetize :cost_square_cents
+  monetize :cost_cash_cents, allow_nil: true
+  monetize :cost_square_cents, allow_nil: true
 
   def self.total_cost_hours
     Duration.new seconds: sum('extract(epoch from cost_hours)').to_f
   end
 
   def cost_hours
-    Duration.new Hash[TIME_PARTS.zip read_attribute(:cost_hours).split(':')]
+    cost_string = read_attribute :cost_hours
+    return nil if cost_string.nil?
+    Duration.new Hash[TIME_PARTS.zip cost_string.split(':')]
   end
 
   def cost_hours=(cost)
