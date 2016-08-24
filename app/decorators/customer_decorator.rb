rustyspoke/rusty-shop

commit 5959552ad5c66d32636218b2150711de7499b124

Use ruby-duration gem
Closes #20

app/decorators/customer_decorator.rb
@@ -30,9 +30,9 @@ class CustomerDecorator < Draper::Decorator
     date == Date.current ? 'Today' : h.l(date)
   end
 
-  def work_trade_available
-    return nil unless object.visits.work_trade.present?
-    h.format_duration(object.total_work_trade_duration - object.total_purchases_cost_hours)
+  def work_trade
+    negative = object.work_trade.negative?
+    (negative ? 'Owes ' : 'Has ') + object.work_trade.format('%h %~h and %m %~m')
   end
 
 end

Gemfile
@@ -31,6 +31,7 @@ gem 'formtastic', '~> 3.0'
 gem 'active_admin_datetimepicker'
 gem 'twilio-ruby', '~> 4.11.1'
 gem 'money-rails'
+gem 'ruby-duration'
 
 gem 'tod'
 

app/admin/customer.rb
@@ -27,7 +27,7 @@ ActiveAdmin.register Customer do
   show do
     attributes_table do
       row :latest_visit
-      row :work_trade_available
+      row :work_trade
       row :email
       row :phone
     end

app/admin/purchase.rb
@@ -6,9 +6,9 @@ ActiveAdmin.register Purchase do
   belongs_to :customer
   config.batch_actions = false
 
-  form title: 'Visit' do |f|
-    panel 'Work trade available' do
-      f.object.customer.decorate.work_trade_available
+  form do |f|
+    panel 'Work trade' do
+      f.object.customer.decorate.work_trade
     end
 
     inputs 'Details' do

app/decorators/purchase_decorator.rb
@@ -19,8 +19,8 @@ class PurchaseDecorator < Draper::Decorator
   end
 
   def cost_hours
-    return nil if object.cost_hours == '00:00:00'
-    object.cost_hours
+    return nil unless object.cost_hours.present?
+    object.cost_hours.format '%h %~h and %m %~m'
   end
 end
 

app/decorators/visit_decorator.rb
@@ -21,7 +21,7 @@ class VisitDecorator < Draper::Decorator
   end
 
   def duration
-    h.format_duration model.duration
+    model.duration.format '%h %~h and %m %~m'
   end
 
   def toolbox

app/decorators/visits_decorator.rb
@@ -8,8 +8,8 @@ class VisitsDecorator < Draper::CollectionDecorator
     object.includes(:customer).count('distinct customer_id')
   end
 
-  def work_trade_earned
-    h.format_duration object.work_trade.total_duration
+  def work_trade
+    object.work_trade.total_duration.format('%h %~h and %m %~m')
   end
 
   def worktrade_customer_names

app/helpers/application_helper.rb
@@ -1,8 +1,4 @@
 module ApplicationHelper
-  def format_duration(duration)
-    Time.at(duration).utc.strftime('%H hrs %M mins')
-  end
-
   # https://github.com/activeadmin/activeadmin/issues/2874#issuecomment-32522163
   def arbre(&block)
     Arbre::Context.new(&block).to_s

commit 0f4cd02b5f52103fb769f33185e960769057b61a

Fix worktrade receipt worker
Broken by 5959552ad5c66d32636218b2150711de7499b124

app/decorators/customer_decorator.rb
@@ -30,9 +30,17 @@ class CustomerDecorator < Draper::Decorator
     date == Date.current ? 'Today' : h.l(date)
   end
 
-  def work_trade
+  def work_trade(third_person = true)
     negative = object.work_trade.negative?
-    (negative ? 'Owes ' : 'Has ') + object.work_trade.format('%h %~h and %m %~m')
+    "#{work_trade_verb third_person, negative}  #{object.work_trade.format('%h %~h and %m %~m')}"
+  end
+
+  def work_trade_verb(third_person, is_negative)
+    if third_person
+      is_negative ? 'Owes' : 'Has'
+    else
+      is_negative ? 'Owe' : 'Have'
+    end
   end
 
 end

app/models/visit_texter.rb
@@ -13,7 +13,7 @@ class VisitTexter
     their_number = visit.customer.phone
     client.messages.create from: our_number, to: their_number, body: <<-BODY.strip_heredoc
     Receipt for #{@visit.duration} worktrade at Rusty Spoke on #{@visit.date}.
-    You have #{@customer.work_trade_available} total.
+    You #{@customer.work_trade false}.
     Ask staff if incorrect.
     BODY
   end

app/views/visit_mailer/worktrade_receipt.html.erb
@@ -19,9 +19,8 @@
     </p>
 
     <p>
-    You now have
-    <b><%= @customer.work_trade_available %></b>
-    work trade available.
+    You now
+    <b><%= @customer.work_trade false %></b>
     </p>
     <p>
     Please contact a member of staff during our next opening hours if you believe this is incorrect.