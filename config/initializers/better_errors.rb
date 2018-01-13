66358444ff60b5185bdb655b558ef1f77d1c912b
Dave Tapley <dukedave@gmail.com>
2018-01-13 11:11:04 -0700

    Allow better_errors repl in Docker
    https://github.com/charliesome/better_errors/issues/349#issuecomment-263832105

---
+++ config/initializers/better_errors.rb
@@ -0,0 +1,5 @@
+if Rails.env.development?
+  BetterErrors::Middleware.allow_ip! '10.0.0.0/8'
+  BetterErrors::Middleware.allow_ip! '172.16.0.0/12'
+  BetterErrors::Middleware.allow_ip! '192.168.0.0/16'
+end
