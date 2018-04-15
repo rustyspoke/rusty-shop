1f339b0026e6d38e08326221c44f980110aa9259
Dave Tapley <dukedave@gmail.com>
2018-04-15 13:17:55 -0700

    Use env for Airbrake

--- config/initializers/airbrake.rb
+++ config/initializers/airbrake.rb
@@ -12,8 +12,8 @@ Airbrake.configure do |c|
   # project_key navigate to your project's General Settings and copy the values
   # from the right sidebar.
   # https://github.com/airbrake/airbrake-ruby#project_id--project_key
-  c.project_id = 134900
-  c.project_key = '81218837d546a292d9373d2a992b5efd'
+  c.project_id = ENV['AIRBRAKE_PROJECT_ID']
+  c.project_key = ENV['AIRBRAKE_API_KEY']
 
   # Configures the root directory of your project. Expects a String or a
   # Pathname, which represents the path to your project. Providing this option
