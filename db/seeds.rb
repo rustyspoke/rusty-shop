36c48a439b08cc1aab72ca21f62f90f38c3f7865
Dave Tapley <dukedave@gmail.com>
2018-04-15 13:24:54 -0700

    Shop agnostic seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,1 @@
-AdminUser.create!(email: 'dave@rustyspoke.org', password: 'password', password_confirmation: 'password')
-AdminUser.create!(email: 'bill@rustyspoke.org', password: 'password', password_confirmation: 'password')
-AdminUser.create!(email: 'brian@rustyspoke.org', password: 'password', password_confirmation: 'password')
-AdminUser.create!(email: 'peter@rustyspoke.org', password: 'password', password_confirmation: 'password')
+AdminUser.create!(email: 'test@example.com', password: 'password', password_confirmation: 'password')
